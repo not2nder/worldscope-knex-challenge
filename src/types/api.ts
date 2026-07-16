4859a2bb80a92df65eafaccf3261f9e9d9962c11
not2nder <brtiodark@gmail.com>
2026-07-16 09:56:40 -0300

    Feat: novos campos do type APIResponse

--- src/types/api.ts
+++ src/types/api.ts
@@ -2,6 +2,11 @@ import type { Country } from "./country"
 
 export type APIResponse = {
   data: {
+    meta: {
+      limit: number;
+      offset: number;
+      total: number;
+    };
     objects: Country[];
   };
 }
