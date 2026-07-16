1786148f718c1e7df323af0f1577b07662584ac5
not2nder <brtiodark@gmail.com>
2026-07-16 11:31:53 -0300

    Feat: Adição da biblioteca tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import App from './App.tsx'
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import CountryPage from './pages/CountryPage.tsx'
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -18,6 +23,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
